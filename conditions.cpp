17fb940e32a2e76429459dc4acd4366f9a854c58
Venu27gopal <143952541+Venu27gopal@users.noreply.github.com>
2023-09-25 03:23:36 +0530

    Add files via upload

---
+++ conditions.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a = 62;
+	
+	if(a == 60){
+		cout<<"Hello i am perfect 60 ";
+		
+		
+	}
+	else{
+		cout<<"Hello i am not perfect 60";
+	}
+	
+	
+}
+
+
+int alpha = 5, beta = 9;
+
+if(alpha==5 || beta==9){
+	
+	cout<<"success!!";
+}
---
+++ operators.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 5;
+	int b = 8;
+	
+	int c;
+	c = a+b;
+	
+	cout<<"Addition of a and b is "<<c<<endl;
+	
+	int d;
+	d = b/a;
+	
+	cout<<"div "<<d<<endl;
+	
+	int r;
+	r = b%a;
+	
+	cout<<"rem "<<r<<endl;
+	
+	bool e;
+	e = b<a;
+	
+	cout<<"boolean e "<<e<<endl; 
+	 
+	
+}
